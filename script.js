a57aa0adfa245db4f4d1fbe9ea5e0d03d7fa3a0c
l0aft3r <l0aft3r@gmail.com>
2024-02-21 16:49:46 +0200

    finished ui

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const results = document.querySelector("#results");
+const score = document.querySelector("#score");
+results.setAttribute('style', 'white-space: pre;');
+
 function capitalise(string) {
     return string.toUpperCase()[0] + string.toLowerCase().slice(-string.length+1);
 }
@@ -29,24 +36,31 @@ function game(){
     let winner;
     let userscore = 0;
     let computerScore = 0;
-    for (i=0; i<5; i++) {
-        playerSelection = prompt('Choose your move!');
-        computerSelection = getComputerChoice();
-        winner = playRound(playerSelection, computerSelection);
-        console.log(winner);
-        if (winner.startsWith('You win!')) {
-            userscore++;
-        } else if (winner.startsWith('You lose!')) {
-            computerScore++
+
+    document.addEventListener('click', (e) => {
+        if (['rock', 'paper', 'scissors'].includes(e.target.getAttribute('id'))) {
+            computerSelection = getComputerChoice();
+            winner = playRound(e.target.getAttribute('id'), computerSelection);
+            results.textContent = winner;
+            if (winner.startsWith('You win!')) {
+                userscore++;
+            } else if (winner.startsWith('You lose!')) {
+                computerScore++;
+            }
+            if (userscore === 5 || computerScore === 5){
+                if (userscore>computerScore) {
+                    results.textContent += '\r\nPlayer won!';
+                    userscore = 0;
+                    computerScore = 0;
+                } else if (computerScore>userscore) {
+                    results.textContent += '\r\nComputer won!';
+                    userscore = 0;
+                    computerScore = 0;
+                }
+            }
+            score.textContent = `${userscore} - ${computerScore}`
         }
-    }
-    if (userscore>computerScore) {
-        console.log('You win!')
-    } else if (computerScore>userscore) {
-        console.log('You lose!')
-    } else {
-        console.log("It's a tie!")
-    }
+    })
 }
 
-game()
+game();
